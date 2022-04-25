8112f143d33a12fadfd5c3ee387fb99a7e92d311
Mohamed Fathi <mohamed.metwally@rasid.com.sa>
2022-04-25 12:18:20 +0200

    add update statement in homeController

--- App/Controllers/HomeController.php
+++ App/Controllers/HomeController.php
@@ -8,6 +8,18 @@ class HomeController extends Controller
 {
     public function index()
     {
+        $user =  $this->db->query('SELECT * from users WHERE id = ? ', 1)->fetch();
+
+        $this->db->data('email', 'mohamed@gmail.com')
+            ->where('id=?', 1)
+            ->update('users');
+
+        pre($user);
+        // echo $this->db->data([
+        //     'first_name' => 'Mohamed',
+        //     "last_name" => '25'
+        // ])->insert('users')->lastId();
+
         $this->response->setHeader('name', 'Mohamed');
         $data['name'] = "Mohamed";
 
