5bcdef33732c73a372413b36658b881dfe772f09
Mohamed Fathi <mohamed.metwally@rasid.com.sa>
2022-04-27 14:22:04 +0200

    test new model in homeController

--- App/Controllers/HomeController.php
+++ App/Controllers/HomeController.php
@@ -8,13 +8,20 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $user =  $this->db->query('SELECT * from users WHERE id = ? ', 1)->fetch();
+        $userModel = $this->load->model('User');
+        pre($userModel->get(2));
+        // $user = $this->db->where('id = ?', 2)->fetchAll('users');
+        // $users = $this->db->fetchAll('users');
+        // pre($this->db->rows());
 
-        $this->db->data('email', 'mohamed@gmail.com')
-            ->where('id=?', 1)
-            ->update('users');
+        // $user =  $this->db->query('SELECT * from users WHERE id = ? ', 1)->fetch();
 
-        pre($user);
+        // $this->db->data('email', 'mohamed@gmail.com')
+        //     ->where('id=?', 1)
+        //     ->update('users');
+
+        // pre($user);
+        // pre($users);
         // echo $this->db->data([
         //     'first_name' => 'Mohamed',
         //     "last_name" => '25'
