24c58c7363083fedd31a2453ecf70c9de8c680d4
Mohamed Fathi <mohamed.metwally@rasid.com.sa>
2022-04-28 09:26:14 +0200

    test url class and assets helper in HomeController

--- App/Controllers/HomeController.php
+++ App/Controllers/HomeController.php
@@ -8,28 +8,8 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $userModel = $this->load->model('User');
-        pre($userModel->get(2));
-        // $user = $this->db->where('id = ?', 2)->fetchAll('users');
-        // $users = $this->db->fetchAll('users');
-        // pre($this->db->rows());
-
-        // $user =  $this->db->query('SELECT * from users WHERE id = ? ', 1)->fetch();
-
-        // $this->db->data('email', 'mohamed@gmail.com')
-        //     ->where('id=?', 1)
-        //     ->update('users');
-
-        // pre($user);
-        // pre($users);
-        // echo $this->db->data([
-        //     'first_name' => 'Mohamed',
-        //     "last_name" => '25'
-        // ])->insert('users')->lastId();
-
-        $this->response->setHeader('name', 'Mohamed');
-        $data['name'] = "Mohamed";
-
-        return  $this->view->render('main/home', $data);
+        //    $this->url->link('/home');
+        //    $this->url->redirectTo('/home/posts/1');
+        echo assets('images/image.png');
     }
 }
